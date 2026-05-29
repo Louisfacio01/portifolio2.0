ed0433bee2b09c67f262b76f7d1c80bd09ae5528
unknown <louisfacio01@gmail.com>
2026-05-28 23:13:38 -0300

    Atualização do portfolio bnts teste3

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
         constructor() {
             this.x = Math.random() * canvas.width;
             this.y = Math.random() * canvas.height;
-            this.size = Math.random() * 2.5 + 1.0;
+            this.size = Math.random() * 1.5 + 1.0;
             this.speedX = Math.random() * 0.4 - 0.2;
             this.speedY = Math.random() * 0.4 - 0.2;
         }
